362c9a9a879f284e1f93659a02667a96ebd4de8f
David Lindsley <lindsleyde@gmail.com>
2024-10-16 22:42:55 -0400

    algorithms

--- Algorithms/AlgorithmTest.cs
+++ Algorithms/AlgorithmTest.cs
@@ -1,19 +1,54 @@
+using System;
 using Xunit;
 
 namespace DeveloperSample.Algorithms
 {
     public class AlgorithmTest
     {
-        [Fact(Skip="Not implemented")]
+        #region GetFactorial
+        [Fact]
         public void CanGetFactorial()
         {
             Assert.Equal(24, Algorithms.GetFactorial(4));
         }
+        [Fact]
+        public void CanGetFactorialOfZero()
+        {
+            Assert.Equal(1, Algorithms.GetFactorial(0));
+        }
+        [Fact]
+        public void CanGetFactorialOfOne()
+        {
+            Assert.Equal(1, Algorithms.GetFactorial(1));
+        }
+        [Fact]
+        public void CanNotGetFactorialOfNegative()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Algorithms.GetFactorial(-1));
+        }
+        #endregion
 
-        [Fact(Skip="Not implemented")]
+        #region FormatSeparators
+        [Fact]
         public void CanFormatSeparators()
         {
             Assert.Equal("a, b and c", Algorithms.FormatSeparators("a", "b", "c"));
         }
+        [Fact]
+        public void CanFormatSeparatorsWithNullParam()
+        {
+            Assert.Equal("", Algorithms.FormatSeparators(null));
+        }
+        [Fact]
+        public void CanFormatSeparatorsWithZeroParams()
+        {
+            Assert.Equal("", Algorithms.FormatSeparators(new string[] { }));
+        }
+        [Fact]
+        public void CanFormatSeparatorsWithOneParam()
+        {
+            Assert.Equal("foo", Algorithms.FormatSeparators(new string[] { "foo" }));
+        }
+        #endregion
     }
 }
--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -1,11 +1,31 @@
 using System;
+using System.Linq;
 
 namespace DeveloperSample.Algorithms
 {
     public static class Algorithms
     {
-        public static int GetFactorial(int n) => throw new NotImplementedException();
+        public static int GetFactorial(int n)
+        {
+            if (n < 0)
+                throw new ArgumentOutOfRangeException(nameof(n), "Argument must be non-negative");
 
-        public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
+            if (n == 0 || n == 1)
+                return 1;
+
+            return n * GetFactorial(n - 1);
+        }
+
+        public static string FormatSeparators(params string[] items)
+        {
+            if (items == null || items.Length == 0)
+                return string.Empty;
+
+            if (items.Length == 1)
+                return items[0];
+
+            // personally I'd have added an Oxford comma ...
+            return string.Join(", ", items.Take(items.Length - 1)) + " and " + items.Last();
+        }
     }
 }
