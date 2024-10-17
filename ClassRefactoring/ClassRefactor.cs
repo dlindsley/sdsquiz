6263679fa1ddb60165b0a31b75eded825100354e
David Lindsley <lindsleyde@gmail.com>
2024-10-17 09:41:16 -0400

    class refactor

--- ClassRefactoring/ClassRefactor.cs
+++ ClassRefactoring/ClassRefactor.cs
@@ -14,43 +14,70 @@ namespace DeveloperSample.ClassRefactoring
 
     public class SwallowFactory
     {
-        public Swallow GetSwallow(SwallowType swallowType) => new Swallow(swallowType);
+        public Swallow GetSwallow(
+            SwallowType type,
+            SwallowLoad load = SwallowLoad.None
+        )
+        {
+            if (type == SwallowType.African)
+                return new AfricanSwallow(load);
+            else if (type == SwallowType.European)
+                return new EuropeanSwallow(load);
+            else
+                throw new ArgumentException("Invalid swallow type", nameof(type));  //fail loud/early
+        }
     }
 
-    public class Swallow
+    public abstract class Swallow
     {
         public SwallowType Type { get; }
         public SwallowLoad Load { get; private set; }
 
-        public Swallow(SwallowType swallowType)
+        public Swallow(SwallowType swallowType, SwallowLoad load = SwallowLoad.None)
         {
             Type = swallowType;
+            Load = load;
         }
 
-        public void ApplyLoad(SwallowLoad load)
+        protected virtual double UnladenVelocity { get; }
+        protected virtual double LadenVelocity { get; }
+
+        public virtual void ApplyLoad(SwallowLoad load)
         {
             Load = load;
+        }
+        public virtual void RemoveLoad()
+        {
+            ApplyLoad(SwallowLoad.None);
         }
 
         public double GetAirspeedVelocity()
         {
-            if (Type == SwallowType.African && Load == SwallowLoad.None)
-            {
-                return 22;
-            }
-            if (Type == SwallowType.African && Load == SwallowLoad.Coconut)
-            {
-                return 18;
-            }
-            if (Type == SwallowType.European && Load == SwallowLoad.None)
-            {
-                return 20;
-            }
-            if (Type == SwallowType.European && Load == SwallowLoad.Coconut)
-            {
-                return 16;
-            }
-            throw new InvalidOperationException();
+            return (Load == SwallowLoad.None)
+                   ? UnladenVelocity
+                   : LadenVelocity;
         }
+    }
+
+    public class AfricanSwallow : Swallow
+    {
+        public AfricanSwallow(SwallowLoad load = SwallowLoad.None)
+            : base (SwallowType.African, load)
+        {
+        }
+
+        protected override double UnladenVelocity => 22;
+        protected override double LadenVelocity => 18;
+    }
+
+    public class EuropeanSwallow : Swallow
+    {
+        public EuropeanSwallow(SwallowLoad load = SwallowLoad.None)
+            : base(SwallowType.European, load)
+        {
+        }
+
+        protected override double UnladenVelocity => 20;
+        protected override double LadenVelocity => 16;  // I thought European swallows were incapable of carrying coconuts?!?
     }
 }
